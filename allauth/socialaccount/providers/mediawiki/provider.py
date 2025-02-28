4e227f987c19ea38c39746a8f1207c9f0fbd647e
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2025-02-28 17:10:45 +0700

    fix(mediawiki): Fix AttributeError regarding non-email MediaWiki account

--- allauth/socialaccount/providers/mediawiki/provider.py
+++ allauth/socialaccount/providers/mediawiki/provider.py
@@ -1,4 +1,5 @@
-from typing import Optional
+from collections.abc import Mapping
+from typing import Any, Optional
 
 from django.conf import settings
 
@@ -31,7 +32,7 @@ class MediaWikiProvider(OAuth2Provider):
     oauth2_adapter_class = MediaWikiOAuth2Adapter
 
     @staticmethod
-    def _get_email(data: dict) -> Optional[str]:
+    def _get_email(data: Mapping[str, Any]) -> Optional[str]:
         if data.get("confirmed_email"):
             return data.get("email")
         return None
@@ -39,7 +40,7 @@ class MediaWikiProvider(OAuth2Provider):
     def extract_uid(self, data):
         return str(data["sub"])
 
-    def extract_extra_data(self, data):
+    def extract_extra_data(self, data: Mapping[str, Any]) -> dict[str, Any]:
         return dict(
             email=self._get_email(data),
             realname=data.get("realname"),
@@ -53,8 +54,11 @@ class MediaWikiProvider(OAuth2Provider):
             name=data.get("realname"),
         )
 
-    def extract_email_addresses(self, data):
-        return [EmailAddress(email=self._get_email(data), verified=True, primary=True)]
+    def extract_email_addresses(self, data: Mapping[str, Any]) -> list[EmailAddress]:
+        # A MediaWiki account may not have email address.
+        if addr := self._get_email(data):
+            return [EmailAddress(email=addr, verified=True, primary=True)]
+        return []
 
 
 provider_classes = [MediaWikiProvider]
