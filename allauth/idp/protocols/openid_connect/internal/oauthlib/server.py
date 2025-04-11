c764efdedd374d4a0586b72e98367e20249f9e82
Raymond Penners <raymond.penners@intenct.nl>
2025-04-11 22:57:57 +0200

    fix(idp): bump access token to 160 bit

--- allauth/idp/protocols/openid_connect/internal/oauthlib/server.py
+++ allauth/idp/protocols/openid_connect/internal/oauthlib/server.py
@@ -1,4 +1,6 @@
-from oauthlib.openid import RequestValidator, Server
+import secrets
+
+from oauthlib.openid import Server
 
 from allauth.idp.protocols.openid_connect import app_settings
 from allauth.idp.protocols.openid_connect.internal.oauthlib.request_validator import (
@@ -6,9 +8,15 @@ from allauth.idp.protocols.openid_connect.internal.oauthlib.request_validator im
 )
 
 
+def generate_token(request) -> str:
+    return secrets.token_hex(20)
+
+
 class OAuthLibServer(Server):
     def __init__(self):
         super().__init__(
+            # 160 bit token is recommended, oauthlib uses less.
+            token_generator=generate_token,
             request_validator=OAuthLibRequestValidator(),
             token_expires_in=app_settings.ACCESS_TOKEN_EXPIRES_IN,
         )
