1571741b235c09e19c1400da4c51799f25ab6ef3
Raymond Penners <raymond.penners@intenct.nl>
2025-04-12 14:39:39 +0200

    fix(idp): bump access token size

--- allauth/idp/protocols/openid_connect/internal/oauthlib/server.py
+++ allauth/idp/protocols/openid_connect/internal/oauthlib/server.py
@@ -9,7 +9,10 @@ from allauth.idp.protocols.openid_connect.internal.oauthlib.request_validator im
 
 
 def generate_token(request) -> str:
-    return secrets.token_hex(20)
+    # oauch.io -- at 20, we get:
+    #    Out of 11 valid authorization responses, the
+    #    average calculated entropy for the access tokens was 144,3 (±7,1) bits
+    return secrets.token_hex(25)
 
 
 class OAuthLibServer(Server):
