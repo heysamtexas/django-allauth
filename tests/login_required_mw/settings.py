5274ed4af730081c166c71930afac66301ad9eeb
Raymond Penners <raymond.penners@intenct.nl>
2025-05-15 14:15:29 +0200

    tests(idp): exclude Ninja from LoginRequiredMiddleware

--- tests/login_required_mw/settings.py
+++ tests/login_required_mw/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = (
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.auth.middleware.LoginRequiredMiddleware",
+    "tests.login_required_mw.middleware.CustomLoginRequiredMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
