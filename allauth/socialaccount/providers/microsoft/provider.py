8eab58ba82bdd487d67137ac093272a5cca5aad9
Nikita Wootten <me@nikita.computer>
2025-05-28 17:03:13 -0400

    feat(microsoft): Add provider_token flow

--- allauth/socialaccount/providers/microsoft/provider.py
+++ allauth/socialaccount/providers/microsoft/provider.py
@@ -1,7 +1,11 @@
+import requests
+
+from allauth.socialaccount.adapter import get_adapter
 from allauth.socialaccount.providers.base import AuthAction, ProviderAccount
 from allauth.socialaccount.providers.microsoft.views import (
     MicrosoftGraphOAuth2Adapter,
 )
+from allauth.socialaccount.providers.oauth2.client import OAuth2Error
 from allauth.socialaccount.providers.oauth2.provider import OAuth2Provider
 
 
@@ -15,6 +19,7 @@ class MicrosoftGraphProvider(OAuth2Provider):
     name = "Microsoft"
     account_class = MicrosoftGraphAccount
     oauth2_adapter_class = MicrosoftGraphOAuth2Adapter
+    supports_token_authentication = True
 
     def get_default_scope(self):
         """
@@ -39,6 +44,15 @@ class MicrosoftGraphProvider(OAuth2Provider):
             last_name=data.get("surname"),
             first_name=data.get("givenName"),
         )
+
+    def verify_token(self, request, token):
+        try:
+            adapter = self.get_oauth2_adapter(request)
+            parsed_token = adapter.parse_token(token)
+            login = adapter.complete_login(request, self.app, parsed_token)
+            return login
+        except (OAuth2Error, requests.RequestException) as e:
+            raise get_adapter().validation_error("invalid_token") from e
 
 
 provider_classes = [MicrosoftGraphProvider]
